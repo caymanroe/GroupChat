a94fbab6a173f59a68c0667ab4eea82850ab0476
Cayman Roe <caymanroe@gmail.com>
2016-02-20 04:14:03 +0000

    Updated form to submit to database

--- newgroup.php
+++ newgroup.php
@@ -18,30 +18,109 @@
 		}
 	}
 
-	$GroupInvalid = "";
+	//Reset Error message
+	$NameError = "";
+	$InviteError = "";
+	$OrgError = "";
+	$DescError = "";
 
-	if (isset($_POST['invite0'])) {
+	if (isset($_POST['name'])) {
 
-		$InviteList = array();
-		$t=0;
-		for ($i=0; $t==0; $i++) { 
-			if (isset($_POST['invite'.$i])) {
-				$InviteList[$i] = $_POST['invite'.$i];
-			} else {
-				$t=1;
-			}
-
-		}
-		
-		/*$Name = strip_tags($_POST['name']);
+		$Name = strip_tags($_POST['name']);
 		$Org = strip_tags($_POST['org']);
 		$Icon = strip_tags($_POST['icon']);
 		$Desc = strip_tags($_POST['desc']);
 		$Privacy = strip_tags($_POST['privacy']);
-		$Invite = strip_tags($_POST['name']);*/
 
-		$GroupInvalid = $output;
+		$Name = mysqli_real_escape_string($con, $Name);
+		$Org = mysqli_real_escape_string($con, $Org);
+		$Icon = mysqli_real_escape_string($con, $Icon);
+		$Desc = mysqli_real_escape_string($con, $Desc);
+		$Privacy = mysqli_real_escape_string($con, $Privacy);
+
+		$NameInvalid = 0;
+		$InviteInvalid = 0;
+		$OrgInvalid = 0;
+		$DescInvalid = 0;
+
+		if ($Name != '') {
+			if (strlen($Name) <= 30){
+				//All good
+			} else {
+				$NameError="Please ensure your group name is less than 30 characters.";
+				$NameInvalid = 1;
+			}
+		} else {
+			$NameError="Please fill in a group name.";
+			$NameInvalid = 1;
+		}
+
+		if ($Org != '') {
+			if (strlen($Org) <= 40) {
+				//All Good
+			} else {
+			$OrgError = "Please ensure your organisation name is less than 40 characters.";
+			$OrgInvalid = 1;
+			}
+		}
+
+		if ($Desc != '') {
+			if (strlen($Desc) <= 300) {
+				//All good
+			} else {
+			$DescError = "Please ensure your organisation name is less than 300 characters.";
+			$DescInvalid = 1;
+			}
+		}
+
+		//Check if first invite field has been filled out
+		if (isset($_POST['invite0']) && $_POST['invite0'] != '') {
+			//Create array
+			$InviteList = array();
+			$t=0;
+			//Keep looping through and storing all invite fields until one is left empty.
+			for ($i=0; $t==0; $i++) { 
+				//Check that field is not empty, if it is, set t=1 (breaking array)
+				if (isset($_POST['invite'.$i]) && $_POST['invite'.$i]!='') {
+					//Strip and prepare email addresses
+					$EmailInvite = strip_tags($_POST['invite'.$i]);
+					$EmailInvite = mysqli_real_escape_string($con, $EmailInvite);
+					//Check email is in correct format
+					if (filter_var($EmailInvite, FILTER_VALIDATE_EMAIL)) {
+						//Add entry to array
+						$InviteList[$i] = $EmailInvite;
+						//All Good
+					} else {
+						//If incorrect email format, set error message
+						$InviteError = "One or more of your email invites is not formatted correctly.";
+						$InviteInvalid = 1;
+					}
+				} else {$t=1;}
+			}
+		}
+
+	if ($NameInvalid == 0 && $InviteInvalid == 0 && $OrgInvalid == 0 && $DescInvalid == 0) {
+		$sql = "INSERT INTO `group` (name, description, adminId, icon, org, private)
+		VALUES ('".$Name."','".$Desc."','".$_SESSION['uid']."','".$Icon."','".$Org."','".$Privacy."')";
+
+		if (mysqli_query($con, $sql)) {
+			$id = mysqli_insert_id($con);
+			$InviteError = "New group ID is ".$id;
+			$relsql = "INSERT INTO `groupmember` (userid, groupid) VALUES ('".$_SESSION['uid']."','".$id."')";
+			if (mysqli_query($con, $relsql)) {
+				//All Done
+			} else {
+				$InviteError = "There was an error processing your request. Please try again in a few minutes.";
+			}
+			
+		} else {
+			$InviteError = "There was an error processing your request. Please try again in a few minutes.";
+		}
 	}
+
+	}
+
+
 
 	
 ?>
@@ -87,7 +166,9 @@
 				<div class="step">
 					<h3>Step 1 - Initial setup</h3>
 					<input class="NewGroupTextInput" type="text" name="name" placeholder="Group Name" />
+					<p id="NameError" class="GroupInvalid"><?php echo $NameError; ?></p>
 					<input class="NewGroupTextInput" type="text" name="org" placeholder="Organisation (Optional)" />
+					<p id="OrgError" class="GroupInvalid"><?php echo $OrgError; ?></p>
 					<label id="StartIconLabel">Chose your group icon</label>
 					<div id="icon-selector">
 
@@ -163,17 +244,18 @@
 						<input id="icon-diamond" type="radio" name="icon" value="icon-diamond" />
 						<label class="icon-label" for="icon-diamond"><i class="icon-diamond"></i></label>
 					</div>
-					<textarea name="desc" placeholder="Group Description"></textarea>
+					<textarea name="desc" placeholder="Group Description (Recommended)"></textarea>
+					<p id="DescError" class="GroupInvalid"><?php echo $DescError; ?></p>
 				</div>
 
 				<div class="step">
 					<h3>Step 2 - Is your group private? (Invite only)</h3>
 					<fieldset id="privacy">
 
-						<input id="public" type="radio" name="privacy" checked="checked" value="public">
+						<input id="public" type="radio" name="privacy" checked="checked" value="0">
 						<label class="privacy-label" for="public"><span></span>Public</label>
 
-						<input id="private" type="radio" name="privacy" value="private">
+						<input id="private" type="radio" name="privacy" value="1">
 						<label class="privacy-label" for="private"><span></span>Private</label>
 
 					</fieldset>
@@ -187,7 +269,7 @@
 						<div id="addInvite"><i class="icon-add"></i></div>
 					</div>
 				</div>
-				<p id="GroupInvalid"><?php echo $GroupInvalid; ?></p>
+				<p id="InviteError" class="GroupInvalid"><?php echo $InviteError; ?></p>
 				<input type="submit" value="Create Group"/>
 			</form>
 		</div>
